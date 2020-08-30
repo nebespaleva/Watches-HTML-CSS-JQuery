a2d0d7e3de5e860e84d4eeeab0d08f3c3e9b2867
Aleksandr Arsenyev <aleksandr.kornushin@yandex.ru>
2020-08-30 16:45:14 +0300

    add consultation form and slider carousel

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1000,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>',
+
+      });
+  });
