eeb7be36c8ef7e7cf5afc7a5248a023b42323d5a
Aleksandr Arsenyev <aleksandr.kornushin@yandex.ru>
2020-09-01 22:08:09 +0300

    add adaptive to slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,43 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1000,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>',
 
-      });
-  });
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false,
+  passive: false,
+  responsive: {
+    767: {
+      items: 1
+    },
+    768: {
+      fixedWidth: 575,
+      items: 1
+    },
+    1024: {
+      fixedWidth: 575,
+      items: 1
+    },
+    1025: {
+      fixedWidth: 575,
+      items: 1
+    },
+    1440: {
+      fixedWidth: 750,
+      items: 1
+    },
+    2560: {
+      fixedWidth: 750,
+      items: 1
+    }
+  }
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
